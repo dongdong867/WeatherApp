53098dd73fd2c48d8ed5a9b9f874e88092d5a542
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-04-13 10:16:24 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Dong on 2024/4/13.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
