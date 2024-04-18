7ec9e9a5aad5cfba1066bcab10004c35094d67ef
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-04-18 11:40:09 +0800

    feat(ForecastView): seperate view into small components

--- Weather/View/Forecast/ForecastView.swift
+++ Weather/View/Forecast/ForecastView.swift
@@ -17,42 +17,9 @@ struct ForecastView: View {
                 ProgressView()
             } else {
                 Group {
-                    VStack {
-                        Text(forecast.location.rawValue)
-                            .padding(8)
-                            .fontWeight(.medium)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(.gray)
-                            }
-                        HStack {
-                            Text(getWeekdayText(from: forecast.weekday).uppercased())
-                            Text(forecast.currentRain)
-                        }
-                        .padding(.top, 12)
-                        Text(forecast.currentTemperature)
-                            .font(.system(size: 60))
-                            .fontWeight(.semibold)
-                    }
+                    currentWeather
                     SplineImage(url: getSplineURLWithImagePath(from: getImagePathFromWeatherType(from: forecast.currentWeatherType)))
-                    HStack {
-                        ForEach(0..<5) { index in
-                            VStack {
-                                Image(getImagePathFromWeatherType(from: forecast.fiveDayWeatherType[index]))
-                                    .resizable()
-                                    .scaledToFit()
-                                Text(getWeekdayText(from: (forecast.weekday + index) % 7 + 1))
-                                    .font(.system(size: 14, weight: .medium))
-                                    .padding(.bottom, 2)
-                                Group {
-                                    Text(forecast.fiveDayTemperature[index])
-                                    Text(forecast.fiveDayRain[index])
-                                }
-                                .font(.system(size: 16))
-                            }
-                        }
-                    }
-                    .padding(.horizontal, 40)
+                    fiveDayForecast
                 }
             }
         }
@@ -69,6 +36,56 @@ struct ForecastView: View {
         } message: {
             Text(forecast.error?.localizedDescription ?? "")
         }
+    }
+    
+    var currentWeather: some View {
+        VStack {
+            locationButton
+            HStack {
+                Text(getWeekdayText(from: forecast.weekday).uppercased())
+                Text(forecast.currentRain)
+            }
+            .padding(.top, 12)
+            Text(forecast.currentTemperature)
+                .font(.system(size: 60))
+                .fontWeight(.semibold)
+        }
+    }
+    
+    var fiveDayForecast: some View {
+        HStack {
+            ForEach(0..<5) { index in
+                VStack {
+                    Rectangle()
+                        .fill(Color.clear)
+                        .aspectRatio(1, contentMode: .fit)
+                        .overlay {
+                            Image(getImagePathFromWeatherType(from: forecast.fiveDayWeatherType[index]))
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    Text(getWeekdayText(from: (forecast.weekday + index) % 7 + 1))
+                        .font(.system(size: 14, weight: .medium))
+                        .padding(.bottom, 2)
+                    Group {
+                        Text(forecast.fiveDayTemperature[index])
+                        Text(forecast.fiveDayRain[index])
+                    }
+                    .font(.system(size: 16))
+                }
+            }
+        }
+        .padding(.horizontal, 40)
+    }
+    
+    var locationButton: some View {
+        Text(forecast.location.rawValue)
+            .padding(8)
+            .fontWeight(.medium)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(.gray)
+            }
     }
 }
 
